a356195e4f7b171036df1969d8113b30a36cfe6e
diadasiachilensis <guillermo.vidal.astudillo@gmail.com>
2025-02-04 14:54:20 -0300

    restudy MVC

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#import de nuestra view
+from app.views import my_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #aca se especifican todas las urls 
+    path('car-list/',my_view)
 ]
